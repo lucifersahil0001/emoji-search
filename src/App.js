b845ce5a8d7670c7729a47abf316bd8325278038
lucifersahil0001 <harshgupta00010@gmail.com>
2023-07-12 00:00:23 +0530

    final-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Component
+} from "react";
+import Header from "./components/Header";
+import InputSearch from "./components/InputSearch"
+import SearchEmoji from "./components/SearchEmoji"
+import SearchResult from "./components/SearchResult"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchEmoji: SearchEmoji("", 600)
+    };
+  }
+
+
+  handleSearchChange = event =>{
+    this.setState({
+      SearchEmoji: SearchEmoji(event.target.value,20)
+    });
+  };
+
+  render (){
+    return (
+      <div>
+      <Header />
+      <InputSearch textChange={this.handleSearchChange} />
+      <SearchResult emojiData={this.state.SearchEmoji} />
+      </div>
+    );
+  }
 }
 
 export default App;
